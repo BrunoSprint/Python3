0a7a56a71e22624a2be367b73538d1491a9319d2
BrunoSprint <117302910+BrunoSprint@users.noreply.github.com>
2023-12-21 00:23:01 -0300

    Create operadores.py

---
+++ operadores.py
@@ -0,0 +1,37 @@
+print(''Hello, Word!'')
+
+
+numeros inteiros sao representados por int (10)
+
+numero de ponto flutuante utiliza-se float (0.777)
+
+valore boleanos ( true - false )
+
+print( 1 + 10 )
+
+print(1.0)
+
+print(''Python'')
+
+int()
+float()
+str()
+bool()
+
+rgb(4, 238, 255)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
