53ed062d554e9530806123ce5cbdb3a51b67ae53
BrunoSprint <brunosprintdev@outlook.com>
2023-12-21 02:14:22 -0300

    .

--- 01.py
+++ 01.py
@@ -5,6 +5,24 @@ def sacar(valor):
         print("valor sacado!")
         print("retire seu dinheiro na boca do caixa.")
 
-    print("obrigado por ser nosso cliente, tenha um bom dia")
+    print("obrigado por ser nosso cliente, tenha um bom dia.")
 
 sacar(100)
+
+# Só é feita atualização na saida quando o arquivo é salvo, então atualiza  a saida.
+
+
+
+
+def depositar(valor):
+    saldo = 500
+    saldo += valor
+
+sacar(1000)
+
+
+
+
+
+
+
