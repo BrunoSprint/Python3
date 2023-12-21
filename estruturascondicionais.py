ea2dfa185970d6fd559f00a7a9638bae49395fc9
BrunoSprint <brunosprintdev@outlook.com>
2023-12-21 02:40:55 -0300

    .

---
+++ estruturascondicionais.py
@@ -0,0 +1,26 @@
+# Argumetação: etapa1: IF / if-else / elif  etapa2: IF aninhado  etapa3: IF ternário
+# A estrutura condicional permite o desvio de fluxo de controle, quando determinadas expressões lógicas são atendidas.
+
+# if simples: cria uma etrutura condicional simples,composta por um unico desvio, podemos utilizar a palavra reservada if.
+# O comando ira testar a expressão lógica, em caso de verdadeiro as ações presentes no bloco de codigo do if serão executadas.
+#exemplo:
+
+saldo = 2000.0
+saque = float(input("Informe o valor do Saque: "))
+
+if saldo >= saque:
+    print("Realizando Saque!")
+
+if saldo <= saque:
+    print("Saldo Insuficiente!")
+
+
+# if / else : Estrutura condicionada com dois devios, palavras reservadas if&else. 
+# Se a Expressão lógica for testada no if for verdadeira, o bloco de código do if será executado, o contrario else será execuado.
+
+
+
+
+
+
+
