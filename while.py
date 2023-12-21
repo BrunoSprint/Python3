930f8bc5846463f8523a86443d0e33f1f3b2a981
BrunoSprint <brunosprintdev@outlook.com>
2023-12-21 20:18:23 -0300

    .

--- while.py
+++ while.py
@@ -1,14 +1,14 @@
 opcao = -1
 
 while opcao != 0:
-    opcao = init(input("[1] Scar \n[2] Extrato \n[0] Sair \n: "))
+    opcao = int(input("[1] Sacar \n[2] Extrato \n[0] Sair \n: "))
 
     if opcao == 1:
         print("Sacando...")
-
     elif opcao == 2:
-        print("Exibindo o Estrato...")
-
+        print("Exibindo o Extrato...")
+else:
+    print("Obrigad por Usar nosso Sistema Bancário, até logo!")
 
 
 
@@ -26,7 +26,9 @@ while opcao != 0:
 """
  while = enquanto
 O comando WHILE é usado para repetir um bloco de código várias vees.  
-Faz sentido usar WHILE quando não sabemos o número exato de vezes que nosso bloco de código deve ser executado.    
+Faz sentido usar WHILE quando não sabemos o número exato de vezes que nosso bloco de código deve ser executado. 
+
+o If testa e executa uma unica vez.
 
 
 """
