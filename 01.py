47e5d8788490fc8eb08d555bb1cab07cf239d678
BrunoSprint <brunosprintdev@outlook.com>
2023-12-21 01:35:44 -0300

    .

--- 01.py
+++ 01.py
@@ -1,3 +1,10 @@
-name = john
+def sacar(valor):
+    saldo = 500
 
-print(name)
+    if saldo >= valor:
+        print("valor sacado!")
+        print("retire seu dinheiro na boca do caixa.")
+
+    print("obrigado por ser nosso cliente, tenha um bom dia")
+
+sacar(100)
