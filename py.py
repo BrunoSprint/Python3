ec519b0bfee220c65084afc7ba286ebb783e573a
BrunoSprint <brunosprintdev@outlook.com>
2023-12-20 23:57:38 -0300

    .

---
+++ py.py
@@ -0,0 +1,29 @@
+# Para criar um comentário em Python de linha unica use o #
+
+""" Para criar um comentaro
+em diferentes linhas use : Aspas triplas, seja simples ou dupla."""
+
+
+
+
+
+# Identar código é uma forma de manter o código fonte mais legível e manutenivél.
+#Mas em Python, através da identação o interpretador consegue determinar onde o bloco de comando inicia e onde termina.
+
+
+# Exemplo de identação Py
+
+def sacar(self, valor: float) none: # inicio do bloco do méodo
+    if self.saldo >= valor: # inicio do bloco if
+        self.saldo -= valor
+
+    #fim do bloco if
+# fim do bloco método
+        
+
+
+
+
+
+
+        
