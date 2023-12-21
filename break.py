a30fd29c59756a295399b71aba07dbb2f3aa9d45
BrunoSprint <brunosprintdev@outlook.com>
2023-12-21 20:33:05 -0300

    .

---
+++ break.py
@@ -0,0 +1,17 @@
+while True:
+    numero = int(input("Informe um número: "))
+
+    if numero == 10:
+        break
+    # no caso, quando atingir o numero 10, havera o BREAK a condição de parada.
+
+    print(numero)
+
+
+
+
+
+
+
+# while True : enquanto verdade, pode criar um laço infinito.
+    
