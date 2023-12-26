f687c58028b52c8b77cb4fb5e803de2dd8d01d35
BrunoSprint <brunosprintdev@outlook.com>
2023-12-26 13:43:05 -0300

    .

---
+++ fatiamentodestring.py
@@ -0,0 +1,22 @@
+nome = "Michael Douglas"
+
+print(nome[0])
+
+print(nome[:9])
+
+print(nome[10:])
+
+print(nome[10:15])
+
+print(nome[10:15:2])
+
+print(nome[:])
+
+print(nome[::-1])
+
+print(nome[-3])
+
+print(nome[-7])
+
+#Fatiamento é uma Técnica de retorno de SUBSTRINGS partes da original, informando: (start)inicio, (stop)fim, (step)passo:
+#[start:stopp[step]]
