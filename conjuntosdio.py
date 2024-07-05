08f02302429005062323c21368a9800728f2a844
Bruno César <117302910+BrunoSprint@users.noreply.github.com>
2024-07-04 21:43:36 -0300

    set

---
+++ conjuntosdio.py
@@ -0,0 +1,16 @@
+''' 
+Criando sets : Um set é uma coleção que não possui objetos repetidos, usamos dets para representar conjuntos matemáticos ou eliminar itens duplicados de um iterável. 
+'''
+
+
+set([1, 2, 3, 1, 3, 4])    #[1, 2, 3, 4]
+
+set("abacaxi")    #{"b", "a", "c", "x", "i"}
+
+
+set(("palio", "gol", "celta", "palio"))  #{"gol", "celta", "palio"}
+
+
+
+
+
